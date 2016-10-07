d9d98669eac71ae15e96d30d039e0ea0c83e1e27
lanayurchuk <urchuk.lana@gmail.com>
2016-10-07 22:07:32 +0300

    Начинаем программировать (#2)
    * Подключение скриптов
    
    * Функция
    
    * Функция исправлена, исходя из замечаний
    
    * Убрала лишний if

---
+++ src/js/check.js
@@ -0,0 +1,31 @@
+function getMessage(a, b) {
+	if (typeof a === 'boolean'){
+		if (a){
+			return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
+		}
+		
+		return 'Переданное GIF-изображение не анимировано';
+	}
+
+	if (typeof a === 'number') {
+		return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов';
+	}
+
+	if (Array.isArray(a) && !(Array.isArray(b))) {
+		var amountOfRedPoints = a.reduce(function(sum, current) {
+			return sum + current;
+		}, 0);
+		return 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints ;
+	}
+	
+	if (Array.isArray(a) && Array.isArray(b)) {
+		var artifactsSquare = a.reduce(function(sum, current, i) {
+			return sum + (current * b[i]);
+		}, 0);
+		return 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
+	}
+	
+	else {
+		return 'Переданы некорректные данные';
+	}
+}
