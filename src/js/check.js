f6913c049d453a6da2ffe6aa349bab326af2f9d6
Lana Yurchuk <urchuk.lana@gmail.com>
2016-10-07 16:19:04 +0300

    Функция исправлена, исходя из замечаний

--- src/js/check.js
+++ src/js/check.js
@@ -1,31 +1,32 @@
-var getMessage = function(a, b) {
-	if (typeof(a) == 'boolean' && a == true) {
-		return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
+function getMessage(a, b) {
+	if (typeof a === 'boolean'){
+		if (a){
+			return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
+		}
+		
+		if (!a){
+			return 'Переданное GIF-изображение не анимировано';
+		}
 	}
-	else if (typeof(a) == 'boolean' && a == false) {
-		return 'Переданное GIF-изображение не анимировано';
-	}
-	else if (typeof(a) == 'number') {
+
+	if (typeof a === 'number') {
 		return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов';
 	}
-	else if (Array.isArray(a) && !(Array.isArray(b))) {
-		var amountOfRedPoints = 0;
-		var i = 0;
-		while (i < a.length) {
-			amountOfRedPoints += a[i]; 
-			i++;
-		}
+
+	if (Array.isArray(a) && !(Array.isArray(b))) {
+		var amountOfRedPoints = a.reduce(function(sum, current) {
+			return sum + current;
+		}, 0);
 		return 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints ;
 	}
-	else if (Array.isArray(a) && Array.isArray(b)) {
-		var artifactsSquare = 0;
-		var i = 0;
-		while (i < a.length) {
-			artifactsSquare += a[i] * b[i];
-			i++;
-		}
+	
+	if (Array.isArray(a) && Array.isArray(b)) {
+		var artifactsSquare = a.reduce(function(sum, current, i) {
+			return sum + (current * b[i]);
+		}, 0);
 		return 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
 	}
+	
 	else {
 		return 'Переданы некорректные данные';
 	}
