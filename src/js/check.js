37b668d11c6b3b2e72a70c5f0693c2b7ffaa0aff
lanayurchuk <urchuk.lana@gmail.com>
2016-10-09 13:57:57 +0300

    Canvas (#3)
    * Доработана функция redraw
    
    * Убрала подключение лишних скриптов
    
    * подправила оформление
    
    * Правки в оформлении кода

--- src/js/check.js
+++ src/js/check.js
@@ -1,31 +1,31 @@
-function getMessage(a, b) {
-	if (typeof a === 'boolean'){
-		if (a){
-			return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
-		}
-		
-		return 'Переданное GIF-изображение не анимировано';
-	}
+'use strict';
 
-	if (typeof a === 'number') {
-		return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов';
-	}
+window.getMessage = function getMessage(a, b) {
+  if (typeof a === 'boolean') {
+    if (a) {
+      return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
+    }
 
-	if (Array.isArray(a) && !(Array.isArray(b))) {
-		var amountOfRedPoints = a.reduce(function(sum, current) {
-			return sum + current;
-		}, 0);
-		return 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints ;
-	}
-	
-	if (Array.isArray(a) && Array.isArray(b)) {
-		var artifactsSquare = a.reduce(function(sum, current, i) {
-			return sum + (current * b[i]);
-		}, 0);
-		return 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
-	}
-	
-	else {
-		return 'Переданы некорректные данные';
-	}
-}
+    return 'Переданное GIF-изображение не анимировано';
+  }
+
+  if (typeof a === 'number') {
+    return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов';
+  }
+
+  if (Array.isArray(a) && !(Array.isArray(b))) {
+    var amountOfRedPoints = a.reduce(function(sum, current) {
+      return sum + current;
+    }, 0);
+    return 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
+  }
+
+  if (Array.isArray(a) && Array.isArray(b)) {
+    var artifactsSquare = a.reduce(function(sum, current, i) {
+      return sum + (current * b[i]);
+    }, 0);
+    return 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
+  }
+
+  return 'Переданы некорректные данные';
+};
