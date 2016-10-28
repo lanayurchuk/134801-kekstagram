0913b76512e17d283497e9fb1c4929f1ad82585e
Lana Yurchuk <urchuk.lana@gmail.com>
2016-10-29 00:38:31 +0300

    Вывод списка изображений

---
+++ src/js/pictures.js
@@ -0,0 +1,34 @@
+'use strict';
+
+(function() {
+  var filters = document.querySelector('.filters');
+  var container = document.querySelector('.pictures');
+  var callbackNamePictures = 'CallbackPictures';
+  var picturesLoadUrl = 'http://localhost:1507/api/pictures?callback=' + callbackNamePictures;
+
+  var getPictureElement = require('./picture');
+  var createCallback = require('./load');
+
+  hide(filters);
+  createCallback(picturesLoadUrl, loadPictures, callbackNamePictures);
+
+  function show(element) {
+    element.classList.remove('hidden');
+  }
+
+  function hide(element) {
+    element.classList.add('hidden');
+  }
+
+  function renderPictures(pictures) {
+    pictures.forEach(function(picture) {
+      var newElement = getPictureElement(picture);
+      container.appendChild(newElement);
+    });
+  }
+
+  function loadPictures(data) {
+    renderPictures(data);
+    show(filters);
+  }
+})();
