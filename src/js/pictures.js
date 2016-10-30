ca3ea73280589fda91ea2180ac2ff6f760c421d2
Lana Yurchuk <urchuk.lana@gmail.com>
2016-10-30 17:10:48 +0200

    Убран IIFE, подключение модулей перенесено в начало файла

--- src/js/pictures.js
+++ src/js/pictures.js
@@ -1,34 +1,32 @@
 'use strict';
 
-(function() {
-  var filters = document.querySelector('.filters');
-  var container = document.querySelector('.pictures');
-  var callbackNamePictures = 'CallbackPictures';
-  var picturesLoadUrl = 'http://localhost:1507/api/pictures?callback=' + callbackNamePictures;
+var getPictureElement = require('./picture');
+var createCallback = require('./load');
 
-  var getPictureElement = require('./picture');
-  var createCallback = require('./load');
+var filters = document.querySelector('.filters');
+var container = document.querySelector('.pictures');
+var callbackNamePictures = 'CallbackPictures';
+var picturesLoadUrl = 'http://localhost:1507/api/pictures?callback=' + callbackNamePictures;
 
-  hide(filters);
-  createCallback(picturesLoadUrl, loadPictures, callbackNamePictures);
+hide(filters);
+createCallback(picturesLoadUrl, loadPictures, callbackNamePictures);
 
-  function show(element) {
-    element.classList.remove('hidden');
-  }
+function show(element) {
+  element.classList.remove('hidden');
+}
 
-  function hide(element) {
-    element.classList.add('hidden');
-  }
+function hide(element) {
+  element.classList.add('hidden');
+ }
 
-  function renderPictures(pictures) {
-    pictures.forEach(function(picture) {
-      var newElement = getPictureElement(picture);
-      container.appendChild(newElement);
-    });
-  }
+function renderPictures(pictures) {
+  pictures.forEach(function(picture) {
+    var newElement = getPictureElement(picture);
+    container.appendChild(newElement);
+  });
+}
 
-  function loadPictures(data) {
-    renderPictures(data);
-    show(filters);
-  }
-})();
+function loadPictures(data) {
+  renderPictures(data);
+  show(filters);
+}
