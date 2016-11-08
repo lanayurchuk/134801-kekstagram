744bd2db5d9f41f91000584e2e199555f81e5411
Lana Yurchuk <urchuk.lana@gmail.com>
2016-11-09 01:46:59 +0200

    Постраничный вывод изображений

--- src/js/pictures.js
+++ src/js/pictures.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var createCallback = require('./load');
+var load = require('./load');
 var Gallery = require('./gallery');
 var Picture = require('./picture');
 
@@ -8,11 +8,25 @@ var gallery = new Gallery();
 
 var filters = document.querySelector('.filters');
 var container = document.querySelector('.pictures');
-var callbackNamePictures = 'CallbackPictures';
-var picturesLoadUrl = 'http://localhost:1507/api/pictures?callback=' + callbackNamePictures;
+var footer = document.querySelector('footer');
+
+var picturesLoadUrl = '/api/pictures';
+
+var activeFilter = 'filter-popular';
+var currentPage = 0;
+var pageSize = 12;
+var throttleTimeout = 100;
+var GAP = 100;
+var lastCall = Date.now();
+
+var params = {
+  from: currentPage * pageSize,
+  to: currentPage * pageSize + pageSize,
+  filter: activeFilter
+};
 
 hide(filters);
-createCallback(picturesLoadUrl, loadPictures, callbackNamePictures);
+load(picturesLoadUrl, params, loadPictures);
 
 function show(element) {
   element.classList.remove('hidden');
@@ -33,4 +47,34 @@ function loadPictures(data) {
   renderPictures(data);
   gallery.setPictures(data);
   show(filters);
+}
+
+window.addEventListener('scroll', function() {
+  if(Date.now() - lastCall >= throttleTimeout) {
+    if (footer.getBoundingClientRect().top - window.innerHeight <= GAP) {
+      params.from = ++currentPage * pageSize;
+      params.to = params.from + pageSize;
+
+      load(picturesLoadUrl, params, loadPictures);
+    }
+    lastCall = Date.now();
+  }
+});
+
+filters.addEventListener('click', function(evt) {
+  if(evt.target.classList.contains('filters-radio')) {
+    changeFilter(evt.target.id);
+  }
+});
+
+function changeFilter(filterName) {
+  container.innerHTML = '';
+  currentPage = 0;
+  activeFilter = filterName;
+
+  params.from = currentPage;
+  params.to = pageSize;
+  params.filter = activeFilter;
+
+  load(picturesLoadUrl, params, loadPictures);
 }
