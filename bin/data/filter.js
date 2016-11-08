8f8de8b7dece9dee2aa8b633deb24a44aa183756
Lana Yurchuk <urchuk.lana@gmail.com>
2016-11-09 01:43:47 +0200

    Сортировка фильтров

--- bin/data/filter.js
+++ bin/data/filter.js
@@ -1,5 +1,22 @@
 'use strict';
 
-module.exports = function(list, filterID) {
+module.exports = function(list, filterName) {
+  switch (filterName) {
+    case 'filter-popular':
+      list.sort(function(a, b) {
+        return b.likes - a.likes;
+      });
+      break;
+    case 'filter-new':
+      list.sort(function(a, b) {
+        return b.created - a.created;
+      });
+      break;
+    case 'filter-discussed':
+      list.sort(function(a, b) {
+        return b.comments - a.comments;
+      });
+      break;
+  }
   return list;
 };
