64998b5fec848a0c8918090ebb34cc118fe0ffcb
Lana Yurchuk <urchuk.lana@gmail.com>
2016-11-10 10:56:32 +0200

    поправлен показ галереи

--- src/js/pictures.js
+++ src/js/pictures.js
@@ -18,6 +18,8 @@ var pageSize = 12;
 var throttleTimeout = 100;
 var GAP = 100;
 var lastCall = Date.now();
+// комбинированный массив со всеми подгружаемыми данными
+var newData = [];
 
 var params = {
   from: currentPage * pageSize,
@@ -27,6 +29,7 @@ var params = {
 
 hide(filters);
 load(picturesLoadUrl, params, loadPictures);
+autoCompletePage();
 
 function show(element) {
   element.classList.remove('hidden');
@@ -38,24 +41,23 @@ function hide(element) {
 
 function renderPictures(pictures) {
   pictures.forEach(function(picture, index) {
-    var newElement = new Picture(picture, index, gallery);
+    var indexPic = index + (pageSize * currentPage);
+    var newElement = new Picture(picture, indexPic, gallery);
     container.appendChild(newElement.element);
   });
 }
 
 function loadPictures(data) {
+  newData = newData.concat(data);
   renderPictures(data);
-  gallery.setPictures(data);
+  gallery.setPictures(newData);
   show(filters);
 }
 
 window.addEventListener('scroll', function() {
   if(Date.now() - lastCall >= throttleTimeout) {
-    if (footer.getBoundingClientRect().top - window.innerHeight <= GAP) {
-      params.from = ++currentPage * pageSize;
-      params.to = params.from + pageSize;
-
-      load(picturesLoadUrl, params, loadPictures);
+    if (isFooterBottom) {
+      showNextPage();
     }
     lastCall = Date.now();
   }
@@ -69,6 +71,7 @@ filters.addEventListener('click', function(evt) {
 
 function changeFilter(filterName) {
   container.innerHTML = '';
+  newData = [];
   currentPage = 0;
   activeFilter = filterName;
 
@@ -77,4 +80,25 @@ function changeFilter(filterName) {
   params.filter = activeFilter;
 
   load(picturesLoadUrl, params, loadPictures);
+  autoCompletePage();
+}
+
+function isFooterBottom() {
+  var footerRect = footer.getBoundingClientRect();
+  var endPage = footerRect.bottom - window.innerHeight;
+  return endPage <= GAP;
+}
+
+function showNextPage() {
+  params.from = ++currentPage * pageSize;
+  params.to = params.from + pageSize;
+  load(picturesLoadUrl, params, loadPictures);
+}
+
+function autoCompletePage() {
+  var clientHeight = document.body.clientHeight;
+  var scrollHeight = document.body.scrollHeight;
+  if (clientHeight < scrollHeight) {
+    showNextPage();
+  }
 }
